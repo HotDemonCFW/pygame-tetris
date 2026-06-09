de500cc8180cd2ac8adca0d805e39d37ae8c779c
HotDemon <127255712+HotDemonCFW@users.noreply.github.com>
2026-06-09 13:51:46 +0200

    Datenbank und controls hinzugefügt.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Bibliothek die wir für das Spiel nutzen.
 import pygame
 import random
+import json
+import os
 
 # Das Grid-Pattern + Info-Panel
 quadrat_größe       = 30    # Die größe in Pixel.
@@ -11,6 +13,12 @@ breite              = spalten * reihen + info_panel
 höhe                = reihen * spalten
 FPS                 = 60    #Damit wir die Geräte nicht überfordern!
 
+# Steuerung einbinden
+with open("controls.json", "r") as file:
+    controls_data = json.load(file)
 
+# Highscore-Liste einbinden
+with open("highscores.json", "r") as file:
+    highscores_data = json.load(file)
 
 if __name__ == '__main__':
